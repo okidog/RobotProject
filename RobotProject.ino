84da681317c8fba0543c27d0fd0e96019468956f
Aubrey <gtracer465@gmail.com>
2025-12-01 09:22:02 -0500

    Fix header inclusion

--- RobotProject.ino
+++ RobotProject.ino
@@ -1,5 +1,41 @@
+#include <Motor.h>
+#include <IRremote.h>
+
+const byte leftMotorPin1 = 4;
+const byte leftMotorPin2 = 5;
+const byte leftMotorPin3 = 6;
+const byte leftMotorPin4 = 7;
+const byte rightMotorPin1 = A0;
+const byte rightMotorPin2 = A1;
+const byte rightMotorPin3 = A2;
+const byte rightMotorPin4 = A3;
+
+const byte servoPin = 9;
+
+const byte IR_RECIEVE_PIN = 3;
+
+class Ultrasonic {
+  private: 
+  	uint8_t trigPin = A4;
+  	uint8_t echoPin = A5;
+  public:
+  void setPins(uint8_t newTrigPin, uint8_t newEchoPin) {
+    trigPin = newTrigPin;
+    echoPin = newEchoPin;
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+  }
+};
+
+Ultrasonic ultrasonic;
+
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(leftMotorPin1, INPUT);
+  pinMode(leftMotorPin2, INPUT);
+  pinMode(leftMotorPin3, INPUT);
+  pinMode(leftMotorPin4, INPUT);
+  pinMode(leftMotorPin1, INPUT);
 
 }
 
